dc5e909a3feca6c29f5d7617d68e48425624fb46
Mo Henriquez <moises.henriquez@ucf.edu>
2013-08-21 19:41:56 -0400

    EOD Updates
    added footer

--- Scripts/main.js
+++ Scripts/main.js
@@ -11,8 +11,21 @@ require.config({
 
 require(['dmenu','easing'], function () {
     
+    // On PageLoad...
+
+
+    screenSizeLabel($(this).width());
+
     //-- 4 -- go to linked header when link is clicked
     $('.featured nav a').bind('click',function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500,'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    $('ul.nav a').bind('click',function(event){
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
@@ -25,50 +38,62 @@ require(['dmenu','easing'], function () {
     if (width < 481) // load mobile scripts
         require([], function () {
             //alert(width + ' - mobile');
-
             //-- Menu3 -----------------------
             $('.dl-menuwrapper').dlmenu({ animationClasses: { classin: 'dl-animate-in-3', classout: 'dl-animate-out-3'} });
-
-            //-- 4 -- go to linked header when link is clicked
-            $('ul.nav a').bind('click',function(event){
-                var $anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
-                }, 1500,'easeInOutExpo');
-                event.preventDefault();
-            });
-
         });
 
     if ((width > 480) && (width < 1025)) // load tablet scripts
         require(['libs/loadMobileCss'], function () {
             //alert(width + " - tablet");
-            
             //-- Menu3 -----------------------
             $('.dl-menuwrapper').dlmenu({ animationClasses: { classin: 'dl-animate-in-3', classout: 'dl-animate-out-3'} });
+            
             //-- 1 -----------------------
             //$('nav#main_menu').mmenu();
-
-            //-- 4 -- go to linked header when link is clicked
-            $('ul.nav a').bind('click',function(event){
-                var $anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
-                }, 1500,'easeInOutExpo');
-                event.preventDefault();
-            });
-
         });
 
     if (width > 1024) // load desktop scripts
         require(['sticky'], function () {
             //alert(width + " - desktop");
-            
             //-- Menu1 -----------------------
             $('.dl-menuwrapper').dlmenu({ animationClasses: { classin: 'dl-animate-in-1', classout: 'dl-animate-out-1'} });
             
             //-- Sticky ---------------------  
-            $(".featured").stick_in_parent();
+            //$(".featured").stick_in_parent();
+
+            $("#aside").stick_in_parent()
+                .on("sticky_kit:stick", function(e) {
+                    //console.log("has stuck!", e.target);
+                })
+                .on("sticky_kit:unstick", function(e) {
+                    //console.log("has unstuck!", e.target);
+            });
+
 
         });
+
+    //-----------------------
+
+
+    // On PageResize...
+
+
+    $(window).resize(function () {
+        screenSizeLabel($(this).width());
+    });
+
+    function screenSizeLabel(width) {
+        width = parseInt(width);
+
+        if (width < 481) // apply mobile scripts
+        $("#message").html(width + " - mobile");
+
+        if ((width > 480) && (width < 1025)) // apply tablet scripts
+        $("#message").html(width + " - tablet");
+
+        if (width > 1024) // apply desktop scripts
+        $("#message").html(width + " - desktop");
+    }
+
+    //-----------------------
 });
